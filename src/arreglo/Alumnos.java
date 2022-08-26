8358f2b125d90ce96be510f35cf9ae781ac83743
eduardo0514 <14.eduardo.watson@gmail.com>
2022-08-25 19:24:01 -0500

    tarea arreglo

--- src/arreglo/Alumnos.java
+++ src/arreglo/Alumnos.java
@@ -9,5 +9,46 @@ package arreglo;
  * @author lenovo
  */
 public class Alumnos {
+     private String nombre;
+    private int edad;
+    private int numero_de_cuenta;
+
+    public Alumnos() {
+    }
+
+    public Alumnos(String nombre, int edad, int numero_de_cuenta) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.numero_de_cuenta = numero_de_cuenta;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getNumero_de_cuenta() {
+        return numero_de_cuenta;
+    }
+
+    public void setNumero_de_cuenta(int numero_de_cuenta) {
+        this.numero_de_cuenta = numero_de_cuenta;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumnos{" + "nombre=" + nombre + ", edad=" + edad + ", numero_de_cuenta=" + numero_de_cuenta + '}';
+    }
     
 }
--- src/arreglo/Arreglo.java
+++ src/arreglo/Arreglo.java
@@ -4,6 +4,11 @@
  */
 package arreglo;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
 /**
  *
  * @author lenovo
@@ -15,6 +20,35 @@ public class Arreglo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Scanner a1 = new Scanner(System.in);
+        
+        Alumnos [] alumno = new Alumnos [5];
+        alumno [0] = new Alumnos(" Pedro", 18, 324567931);
+        alumno [1] = new Alumnos(" Luis", 22, 325678901);
+        alumno [2] = new Alumnos(" Jose", 19, 326789123 );
+        alumno [3] = new Alumnos(" Mateo", 19, 311234870 );
+        alumno [4] = new Alumnos(" Alejandro", 18, 312710960);
+        
+        for (Alumnos estudiante : alumno) {
+            System.out.println( estudiante );
+        }
+        System.out.println("-----");
+
+        for(int i=0; i<alumno.length ; i++){
+            
+        }
+        System.out.println(alumno.length);
+            
+        System.out.println("-----");    
+        
+        System.out.println("Ingrese el nombre de la persona: ");
+        
+        Array.set(args, 0, alumno);
+      
+        
+         
     }
+    
     
 }
